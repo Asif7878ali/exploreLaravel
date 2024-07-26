4d58f75cf490b920ff9e43e28c16713067ef7ef5
AsifRiseofPower <aloasif31@gmail.com>
2024-07-26 22:20:16 +0530

    coror

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,97 +5,44 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
     @section('main-section')
-    <div id="default-carousel" class="relative w-full" data-carousel="slide">
-        <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-             <!-- Item 1 -->
-            <div class=" duration-700 ease-in-out" data-carousel-item>
-                <img src="https://images.unsplash.com/photo-1628336707625-bf8e9eb250dd?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 2 -->
-            <div class=" duration-700 ease-in-out" data-carousel-item>
-                <img src="https://images.unsplash.com/photo-1628336696238-9e479327030a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="...">
-            </div>
-            <!-- Item 3 -->
-            <div class=" duration-700 ease-in-out" data-carousel-item>
-                <img src="https://images.unsplash.com/photo-1715514922735-f5020e67f98a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="...">
-            </div>
-            <!-- Item 4 -->
-            <div class=" duration-700 ease-in-out" data-carousel-item>
-                <img src="https://images.unsplash.com/flagged/photo-1555992939-a8b868d0e237?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="...">
-            </div>
-            <!-- Item 5 -->
-            <div class=" duration-700 ease-in-out" data-carousel-item>
-                <img src="https://unsplash.com/photos/a-lighthouse-with-a-light-on-top-of-it-NJh32izElQw" alt="...">
-            </div>
-        </div>
-        <!-- Slider indicators -->
-        <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-        </div>
-        <!-- Slider controls -->
-        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                </svg>
-                <span class="sr-only">Previous</span>
-            </span>
-        </button>
-        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                </svg>
-                <span class="sr-only">Next</span>
-            </span>
-        </button>
+  <!-- Slideshow container -->
+<div class="slideshow-container">
+
+    <!-- Full-width images with number and caption text -->
+    <div class="mySlides fade">
+      <div class="numbertext">1 / 3</div>
+      <img src="https://unsplash.com/photos/a-man-standing-on-top-of-a-mountain-overlooking-a-valley-DAJqKgAIeAQ" style="width:100%">
+      <div class="text">Caption Text</div>
     </div>
-
-    <script>
-        $(document).ready(function() {
-            var currentIndex = 0;
-            var items = $('[data-carousel-item]');
-            var itemAmt = items.length;
-
-            function cycleItems() {
-                var item = $('[data-carousel-item]').eq(currentIndex);
-                items.hide();
-                item.css('display', 'block');
-            }
-
-            $('[data-carousel-next]').click(function() {
-                currentIndex += 1;
-                if (currentIndex > itemAmt - 1) {
-                    currentIndex = 0;
-                }
-                cycleItems();
-            });
-
-            $('[data-carousel-prev]').click(function() {
-                currentIndex -= 1;
-                if (currentIndex < 0) {
-                    currentIndex = itemAmt - 1;
-                }
-                cycleItems();
-            });
-
-            $('[data-carousel-slide-to]').click(function() {
-                var index = $(this).data('carousel-slide-to');
-                currentIndex = index;
-                cycleItems();
-            });
-
-            cycleItems();
-        });
-    </script>
+  
+    <div class="mySlides fade">
+      <div class="numbertext">2 / 3</div>
+      <img src="https://unsplash.com/photos/a-woman-standing-on-top-of-a-tennis-court-holding-a-racquet-kjXpmeqUYsU" style="width:100%">
+      <div class="text">Caption Two</div>
+    </div>
+  
+    <div class="mySlides fade">
+      <div class="numbertext">3 / 3</div>
+      <img src="https://plus.unsplash.com/premium_photo-1670020264865-43ab698fa581?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" style="width:100%">
+      <div class="text">Caption Three</div>
+    </div>
+  
+    <!-- Next and previous buttons -->
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+  </div>
+  <br>
+  
+  <!-- The dots/circles -->
+  <div style="text-align:center">
+    <span class="dot" onclick="currentSlide(1)"></span>
+    <span class="dot" onclick="currentSlide(2)"></span>
+    <span class="dot" onclick="currentSlide(3)"></span>
+  </div>
 
   @endsection
 </body>
